dc5f8c2da6d4e54e3f76eac42de7c9fce8b00e2e
ozenc <HuseyinOzenc.Taskin@ pennmedicine.upenn.edu>
2022-10-03 12:06:42 -0400

    Adding some more comments to the model

--- code/modelSimulation.m
+++ code/modelSimulation.m
@@ -2,7 +2,7 @@ clear all
 clc
 
 % We need Kendrick's knkutils toolbox. Check if it exists. Pull if it
-% doesn't.
+% doesn't. Then, add path to it.
 currentScriptPath = matlab.desktop.editor.getActiveFilename;
 matlabPath = currentScriptPath(1:end-46);
 knkutilsFolder = fullfile(matlabPath, 'toolboxes', 'knkutils');
@@ -12,7 +12,7 @@ if ~isfolder(knkutilsFolder)
 end
 addpath(genpath(knkutilsFolder));
 
-% Load glare, halo, stripe, uniform stim and concatanate across time
+% Load glare, halo, stripe, uniform stimulus and concatanate across time
 stimulusPath = fullfile(matlabPath, 'projects', 'mtrpGlarePupil', 'code', 'stimulus');
 stimulusFiles = dir(stimulusPath);
 stimulusFiles(1:2) = [];
@@ -22,50 +22,66 @@ for ii = 1:length(stimulusFiles)
     stimulus(:,:,ii) = ss;
 end
 
-% Specify typical values for V1, V2, V3, and hv4 areas
+% Specify typical values for V1, V2, V3, and hv4 areas. These values were
+% taken from http://kendrickkay.net/socmodel/. Scroll down to 
+% "Simulations of the SOC model" section for reference.
 c = [0.9276, 0.9928, 0.9941, 0.9472]; % Strength of second order contrast 
 n = [0.1814, 0.1285, 0.1195, 0.1152]; % Exponent of power-law linearity
 sd = [0.9308, 1.0738, 1.4671, 2.1242]; % SD of 2D gaussian applied to image
+
+% Setting spacing to 1/sd. Is this correct? 
 spacing = [1/sd(1), 1/sd(2), 1/sd(3), 1/sd(4)]; % Number of sd that separate 2D gaussians 
 
-% Specify save names and struct containers
+% Stimulus related values. 
+res = 150; % Number of pixels to resize (save computational time)
+minMaxMean = [0 1 0.5]; % min max mean luminance. Used to rescale stimulus.
+efactor = 1.2; % Number of enlargement to perform for padding. 
+
+% Initialize save names and struct containers
 responseSaveNames = {'response1', 'response2', 'response3', 'response4'};
 cacheSaveNames = {'cache1', 'cache2', 'cache3', 'cache4'};
 responses = [];
 caches = [];
 gammaResponsesMean = [];
+
 %% Run models 
 for ii = 1:length(responseSaveNames)
     %% SOC model, Kay - BOLD
     % Initiate caches
     caches.(cacheSaveNames{ii}) = [];
     
-    % Run SOC Model which gives us the BOLD.
-    [responses.BOLD.(responseSaveNames{ii}),caches.(cacheSaveNames{ii})] = socmodel(stimulus,150,[0 1 0.5],1.2,{37.5 -1 1 8 2 .01 2 0}, ...
+    % Run SOC Model using Kendrick's script.
+    [responses.BOLD.(responseSaveNames{ii}),caches.(cacheSaveNames{ii})] = socmodel(stimulus,res,minMaxMean,efactor,{37.5 -1 1 8 2 .01 2 0}, ...
                                                                                1,.5,sd(ii),spacing(ii),n(ii),c(ii),caches.(cacheSaveNames{ii})); 
                                                                            
     %% SOV model, Hermes - Gamma                                                          
-    % Get stimulus2 stage from SOC image cache. This contains gabor fitted
-    % images. We get these and re-calculate complex-cell energy model
-    % again. This is where the SOV model deviates from the SOC model
+    % Get stimulus2 stage from SOC simulation. This step contains gabor
+    % filtered images. We get these and re-calculate complex-cell energy
+    % model here again. This is where the SOV model deviates from the SOC
+    % model. The resulting image is images x orientations*positions
     gaborFittedStim = caches.(cacheSaveNames{ii}).stimulus2;
     complexCellStimulus = sqrt(blob(gaborFittedStim.^2,2,2));
     
-    % Set number of gabor orientation and get the resolution.
+    % Set number of gabor orientations we used to find the resolution.
+    % This is hardcoded into the socmodel input above. 
+    % See {37.5 -1 1 8 2 .01 2 0}. We then find the resolution of the stim.
     nrGaborOrientations = 8;
     res = sqrt(size(complexCellStimulus,2)/nrGaborOrientations);
     
-    % Commented out below is the original model for Gamma calculation. 
+    % Commented out below is the original SOV model for Gamma calculation.
+    % from Hermes' script.
     % The formula is response = gain * var(gaborFilteredStim * 2DpRF).^powerLawPower
+    
     % modelfun = @(pp,dd) pp(4) * var(reshape(reshape(dd,[],res*res) * gaufun1(pp),[],nrOrientations),[],2).^pp(5);
     
-    % Create a set of gaussian filters, make full. Check this with Geoff.
+    % Create a set of gaussian filters using a Kendrick function, make full. 
+    % Check this with Geoff.
     [d,d,d,d,filters] = applymultiscalegaussianfilters(randn(1,res^2),[sd(ii)],[1/sd(ii)],.01,2);
     filters = full(filters);
 %     nz = filters ~= 0;
   
     % Loop through the pRF filters and calculate the model for each pRF.
-    % We hardcode the prf gain or to 1. And pass the same power-law values 
+    % Hardcode the prf gain to 1. And pass the same power-law values 
     % that we used for the SOC model. We add the results into a one big
     % vector.
     modelfit = [];
@@ -78,15 +94,15 @@ for ii = 1:length(responseSaveNames)
     combined = [];
     combined = [combined modelfit(1:4:length(modelfit)) modelfit(2:4:length(modelfit)) modelfit(3:4:length(modelfit)) modelfit(4:4:length(modelfit))];
     
-%     % Get the mean across PRFs
+%     % Get the mean gamma across PRFs
 %     gammaResponsesMean(:,ii) = mean(combined)';
 
-    % Reshape the vectors into images and add it into the gamma
+    % Reshape the vectors into images and add it into the responses struct.
     F = sqrt(size(combined',2));
     responses.gamma.(responseSaveNames{ii}) = reshape(combined,F,F,[]);
 end                
 
-% visualize the results for BOLD
+% Plot the results for BOLD
 f = figure; setfigurepos([100 100 700 300]);
 subplotCounter = 0;
 stimNames = {'glare', 'halo', 'stripe', 'uniform'};
@@ -107,11 +123,10 @@ end
 sgtitle('SOC model - BOLD, Kay') 
 
 % Do the same for gamma
-% visualize the results for BOLD
 f2 = figure; setfigurepos([100 100 700 300]);
 subplotCounter = 0;
 stimNames = {'glare', 'halo', 'stripe', 'uniform'};
-fprintf('Plotting SOC model - Gamma.... \n')
+fprintf('Plotting SOV model - Gamma.... \n')
 for p=1:4
   subplot(4,5,subplotCounter + 1);
   imagesc(stimulus(:,:,p),[0 1]); axis image tight; colormap(gray); colorbar; title(stimNames{p});
